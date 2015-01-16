1d48b91878a16b36d32ee88757c7442284ea890d
antoine <vegetoine@free.fr>
2015-01-16 18:33:52 +0100

    [New] Added base project generated by default.

---
+++ Noraneko/Source/Noraneko/Private/Noraneko.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#include "Noraneko.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Noraneko, "Noraneko" );
+ 
---
+++ Noraneko/Source/Noraneko/Public/NoranekoGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "NoranekoGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANoranekoGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	ANoranekoGameMode(const FObjectInitializer& ObjectInitializer);
+};
+
+
+
